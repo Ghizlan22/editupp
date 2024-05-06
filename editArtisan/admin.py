7c074a21f83e35096a0701c1e8c2fdaa6475d607
Ghizlan22 <161063646+Ghizlan22@users.noreply.github.com>
2024-05-06 15:10:08 +0200

    Create admin.py

---
+++ editArtisan/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Artisan)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(Satisfaction)
+admin.site.register(WorkHours)
+admin.site.register(Profile)
